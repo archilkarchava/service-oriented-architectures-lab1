f35d791364a9c8852b6be07342785f708f48c460
Archil Karchava <archil1997@gmail.com>
2019-12-09 10:06:31 +0500

    improved regex

--- packages/server/src/modules/countWords.ts
+++ packages/server/src/modules/countWords.ts
@@ -1,7 +1,7 @@
 export const countWords = (s: string) => {
     if (s.length === 0) return []
     const res = new Map<string, number>();
-    for (const word of s.split(/ |\n/)) {
+    for (const word of s.split(/ |\n|\,|\.|\!|\?|\;|\:/)) {
         if (word.length === 0) continue;
         res.set(word, res.get(word) ? res.get(word) + 1 : 1);
     }
